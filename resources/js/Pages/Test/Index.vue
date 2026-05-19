807ae2b83db5baf16d01a22c549f3b46752ffd2c
Naufal Azzmi <naufal.chroome@gmail.com>
2026-05-19 09:25:04 +0800

    init Inertia3, vue3, Tailwind

--- resources/js/Pages/Test/Index.vue
+++ resources/js/Pages/Test/Index.vue
@@ -1,14 +1,23 @@
-<script setup>
-import Layout from './Layout'
-import { Head } from '@inertiajs/vue3'
+<script>
+import { ref } from 'vue';
 
-defineProps({ user: Object })
+export default {
+    setup() {
+        const count = ref(0);
+
+        return { count };
+    }
+}
 </script>
 
 <template>
-    <Layout>
-        <Head title="Welcome" />
-        <h1>Welcome</h1>
-        <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
-    </Layout>
+    <div class="flex flex-col items-center justify-center h-screen bg-gray-100 gap-4">
+        <h1 class="text-3xl font-bold text-amber-600">
+            Hello World!
+        </h1>
+
+        <button @click="count++" class="px-4 py-2 bg-amber-500 text-white rounded">
+            Count is: {{ count }}
+        </button>
+    </div>
 </template>
