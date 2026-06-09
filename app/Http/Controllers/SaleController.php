e2a531f2ade0684e9536a34487df627760f2b9c2
NMahogra <aptabhamakerti@gmail.com>
2026-06-09 10:36:40 +0700

    Mengganti Blade index dengan Vue index, dan mengedit return view pada SaleController

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Sale;
 use Illuminate\Http\Request;
+use Inertia\Inertia;  
 
 class SaleController extends Controller
 {
-    public function index(Request $request)
-    {
-        $sort = $request->get('sort', 'desc');
+   public function index(Request $request)
+{
+    $sort = $request->get('sort', 'desc');
 
-        $sales = Sale::with('book')
-            ->orderBy('total_price', $sort)
-            ->get();
+    $sales = Sale::with('book')
+        ->orderBy('total_price', $sort)
+        ->get();
 
-        return view('sales.index', compact('sales'));
-    }
+    return Inertia::render('Sales/Index', [ //memanggil file Index.vue yang berada di folder Sales
+        'sales' => $sales, //mengirim data penjualan ke view
+        'sort' => $sort, //mengirim informasi tentang urutan sort ke view
+    ]);
+}
 }
---
+++ resources/js/Pages/Sales/Index.vue
@@ -0,0 +1,43 @@
+<script setup> //menggunakan script setup untuk mendefinisikan props yang akan diterima dari controller
+defineProps({ 
+    sales: Array, //mendefinisikan props sales yang akan menerima data penjualan dalam bentuk array
+    sort: String //mendefinisikan props sort yang akan menerima informasi tentang urutan sort dalam bentuk string
+})
+</script>
+
+<template>
+    <div>
+        <h1>Daftar Penjualan Buku</h1>
+
+        <a href="/sales?sort=desc">
+            Penjualan Terbesar
+        </a>
+
+        |
+
+        <a href="/sales?sort=asc">
+            Penjualan Terkecil
+        </a>
+
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Nama Buku</th>
+                    <th>Qty</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr
+                    v-for="sale in sales"
+                    :key="sale.id"
+                >
+                    <td>{{ sale.book.title }}</td>
+                    <td>{{ sale.quantity }}</td>
+                    <td>{{ sale.total_price }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
